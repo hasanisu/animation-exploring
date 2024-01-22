03d7a70e10c45250eab2703e20183d0e87815a34
hasanisu <hasanjimam@gmail.com>
2024-01-22 17:59:29 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import LoadingAnimation from "./assets/LoadingAnimation";
+
+function App() {
+  // return <div className="box-1"></div>;
+  return <LoadingAnimation />;
+}
+
+export default App;
---
+++ src/assets/LoadingAnimation.tsx
@@ -0,0 +1,13 @@
+import "./LoadingAnimation.css";
+const LoadingAnimation = () => {
+  return (
+    <div className="main">
+      <div className="loading">
+        <div className="glass"></div>
+        <div className="box"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingAnimation;
